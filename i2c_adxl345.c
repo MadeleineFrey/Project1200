66c7a0f1f62e098eccae9aaca6a4f31cd6278cb0
Erica <ericaengborg@gmail.com>
2024-02-26 16:02:04 +0100

    modified:   i2c_adxl345.c

--- i2c_adxl345.c
+++ i2c_adxl345.c
@@ -1,6 +1,7 @@
 #include <pic32mx.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include "adxl.h"
 #include "mipslab.h"  
 
@@ -8,18 +9,35 @@ int ADXL_VALUE=0; //This is the signal from the accelerometer
 
 /* Wait for I2C bus to become idle. Checks if any of the ACKEN, RCEN, PEN, RSEN, SEN bits are set to 1 and if the TRSTAT bit are set to 1.
 If any of the bits are set to 1, an I2C operation is in progress. If the TRSTAT bit is set a tranmit operation is in progress. */
-void i2c_idle() {
-	//timer4_conf(0.1);
-	//timer4Start();
-	int count = 0;
-	while(I2C1CON & 0x1F || I2C1STAT & (1 << 14));
+void i2c_wait() {
 
+    if (!(I2C1CON & 0x1F) && !(I2C1STAT & (1 << 14))) {}
+	else {
+		int count = 0;
+        timer4_conf(0.1);
+		timer4Start();
+        while (I2C1CON & 0x1F || I2C1STAT & (1 << 14)){
+			if (IFS(0) & TMR4_FLAG)
+			{
+				IFS(0) &= ~TMR4_FLAG;
+				if (count%10 == 0)
+				{
+					display_clear();
+					display_string(0, "ERROR");
+					wait_2();
+				}
+				
+			}
+		}
+
+    }
+	
 }
 
 /* First, the functions checks so no other operation is on going on the i2c bus. Then it clears the ACKDT bit which is preparing the master to send an acknowledge bit to the slave.
 Then it sets the ACKEN bit. This sends the previously set acknowledge bit to the slave. */
 void i2c_ack() {
-	i2c_idle();
+	i2c_wait();
 	I2C1CONCLR = 1 << 5; //ACKDT = 0
 	I2C1CONSET = 1 << 4; //ACKEN = 1
 }
@@ -27,7 +45,7 @@ void i2c_ack() {
 /* First, the functions checks so no other operation is on going on the i2c bus. Then it sets the ACKDT bit which is preparing the master to send an not-acknowledge bit to the slave.
 Then it sets the ACKEN bit. This sends the previously set not-acknowledge bit to the slave. */
 void i2c_nack() {
-	i2c_idle();
+	i2c_wait();
 	I2C1CONSET = 1 << 5; //ACKDT = 1
 	I2C1CONSET = 1 << 4; //ACKEN = 1
 }
@@ -35,23 +53,23 @@ void i2c_nack() {
 /* Leaves the SCL signal high and pulls the SDA signal low. By that the recieving device knows a message is about to start. 
 The bit generates a start condition and it is wrong to say that it is cleared but after address is sent it is "cleared"*/
 void i2c_start() {
-	i2c_idle();
+	i2c_wait();
 	I2C1CONSET = 1 << 0; //SEN
-	i2c_idle();
+	i2c_wait();
 }
 
 /* This is a restart condition similar to the start condition. */
 void i2c_restart() {
-	i2c_idle();
+	i2c_wait();
 	I2C1CONSET = 1 << 1; //RSEN
-	i2c_idle();
+	i2c_wait();
 }
 
 /* The stop condition does the opposite to a start condition. */
 void i2c_stop() {
-	i2c_idle();
+	i2c_wait();
 	I2C1CONSET = 1 << 2; //PEN
-	i2c_idle();
+	i2c_wait();
 }
 
 /*Since i2c doesn't use negative values it's most safe to use unsigned integers.
@@ -73,18 +91,18 @@ uint8_t i2c_init(void){
 
 /* First, check if the i2c bus is used by another i2c operation, then it send the data to the transmitt register in the i2c buss. After that tha acknowledge status is returned.
 If the reciever sends an acknowledgebit the ACKSTAT bit is 0, of it is no acknowledged the ACKSTAT bit is 1.*/
-bool i2c_send(uint8_t data) {
-	i2c_idle();
+bool i2c_transmitt(uint8_t data) {
+	i2c_wait();
 	I2C1TRN = data; //The data that's transmitted
-	i2c_idle();	//A very important step to make sure that the transmitt operation is finished before crecieving the acknowledgement bit
+	i2c_wait();	//A very important step to make sure that the transmitt operation is finished before crecieving the acknowledgement bit
 	return !(I2C1STAT & (1 << 15));
 }
 
 /* First, the recieve enable bit is set. After that the recieve register from the I2C communication is returned, */
-uint8_t i2c_recv() {
-	i2c_idle();
+uint8_t i2c_recieve() {
+	i2c_wait();
 	I2C1CONSET = 1 << 3; //RCEN = 1
-	i2c_idle();
+	i2c_wait();
 	//I2C1STATCLR = 1 << 6; //I2COV = 0 TA BORT DENNA???
 	return I2C1RCV;
 }
@@ -98,50 +116,50 @@ void adxl_init (void){
 	The while loop wait until the acknoledgebit is recieved.*/
 	do {
 		i2c_start();
-	} while(!i2c_send(ADXL345_ADDRESS << 1));
-    i2c_send(POWER_CTL);
+	} while(!i2c_transmitt(ADXL345_ADDRESS << 1));
+    i2c_transmitt(POWER_CTL);
     // measurer enable bit in the power control register
-    i2c_send(0x08);
+    i2c_transmitt(0x08);
     i2c_stop();
 
 	/*Send write information to the data format register about the resolution*/
 	do {
 		i2c_start();
-	} while(!i2c_send(ADXL345_ADDRESS << 1));
-    i2c_send(DATA_FORMAT);
-    i2c_send(0x0); //+-2 g resolution 10 bit mode
+	} while(!i2c_transmitt(ADXL345_ADDRESS << 1));
+    i2c_transmitt(DATA_FORMAT);
+    i2c_transmitt(0x0); //+-2 g resolution 10 bit mode
     i2c_stop();
 
 /*Set the baud rate*/
 	do {
 		i2c_start();
-	} while(!i2c_send(ADXL345_ADDRESS << 1));
-    i2c_send(BW_RATE);
-    i2c_send(0x0A); // 100 Hz datarate
+	} while(!i2c_transmitt(ADXL345_ADDRESS << 1));
+    i2c_transmitt(BW_RATE);
+    i2c_transmitt(0x0A); // 100 Hz datarate
     i2c_stop();
 
 /*Set offset for the y-axis*/
 	do {
 		i2c_start();
-	} while(!i2c_send(ADXL345_ADDRESS << 1));
-    i2c_send(OFSY);
-    i2c_send(0x20); // 1 and 255. 0 is idle
+	} while(!i2c_transmitt(ADXL345_ADDRESS << 1));
+    i2c_transmitt(OFSY);
+    i2c_transmitt(0x20); // 1 and 255. 0 is idle
     i2c_stop();
 
 /*Set offset for the x-axis*/
 	do {
 		i2c_start();
-	} while(!i2c_send(ADXL345_ADDRESS << 1));
-    i2c_send(OFSX);
-    i2c_send(0x10); // 
+	} while(!i2c_transmitt(ADXL345_ADDRESS << 1));
+    i2c_transmitt(OFSX);
+    i2c_transmitt(0x10); // 
     i2c_stop();
 
 /*Set offset for the z-axis*/
 	do {
 		i2c_start();
-	} while(!i2c_send(ADXL345_ADDRESS << 1));
-    i2c_send(OFSZ);
-    i2c_send(0x6); // 
+	} while(!i2c_transmitt(ADXL345_ADDRESS << 1));
+    i2c_transmitt(OFSZ);
+    i2c_transmitt(0x6); // 
     i2c_stop();
 }
 
@@ -151,19 +169,19 @@ void yaxis_data (int * data){
 /*First, a write condition is sent to the accelerometer telling it that we want to access the DATAY0 register*/
 		do {
 			i2c_start();
-		} while(!i2c_send(ADXL345_ADDRESS << 1)); // 0 write mode
-		i2c_send(DATAY0);
+		} while(!i2c_transmitt(ADXL345_ADDRESS << 1)); // 0 write mode
+		i2c_transmitt(DATAY0);
 
 /*Then, a read condition is sent to the accelerometer*/
 		do {
 			i2c_start();
-		} while(!i2c_send((ADXL345_ADDRESS << 1) | 1)); //1 read mode
+		} while(!i2c_transmitt((ADXL345_ADDRESS << 1) | 1)); //1 read mode
 		
 /*Now, we can start recieving data from the DATAY0 register*/
-		*data = i2c_recv(); //LSB
+		*data = i2c_recieve(); //LSB
 /*By sending an acknowledgebit and then sending a recieve message again we automatically can read from the next register, in this case DATAY1*/
 		i2c_ack();
-		*data |= i2c_recv() << 8; //MSB
+		*data |= i2c_recieve() << 8; //MSB
 
 /*A nack condition is sent to the slave device to tell it that we don't expect more data. After that we stop the transmission to make the I2C bus idle*/
 		i2c_nack();
@@ -178,17 +196,17 @@ void xaxis_data (int * data){
 
 		do {
 			i2c_start();
-		} while(!i2c_send(ADXL345_ADDRESS << 1)); 
-		i2c_send(DATAX0);
+		} while(!i2c_transmitt(ADXL345_ADDRESS << 1)); 
+		i2c_transmitt(DATAX0);
 
 		do {
 			i2c_start();
-		} while(!i2c_send((ADXL345_ADDRESS << 1) | 1)); 
+		} while(!i2c_transmitt((ADXL345_ADDRESS << 1) | 1)); 
 		
 
-		*data = i2c_recv(); //LSB
+		*data = i2c_recieve(); //LSB
 		i2c_ack();
-		*data |= i2c_recv() << 8; //MSB
+		*data |= i2c_recieve() << 8; //MSB
 
 		i2c_nack();
 		i2c_stop();
@@ -200,16 +218,16 @@ void zaxis_data (int * data){
 
 		do {
 			i2c_start();
-		} while(!i2c_send(ADXL345_ADDRESS << 1)); 
-		i2c_send(DATAZ0);
+		} while(!i2c_transmitt(ADXL345_ADDRESS << 1)); 
+		i2c_transmitt(DATAZ0);
 
 		do {
 			i2c_start();
-		} while(!i2c_send((ADXL345_ADDRESS << 1) | 1)); 
+		} while(!i2c_transmitt((ADXL345_ADDRESS << 1) | 1)); 
 		
-		*data = i2c_recv(); 
+		*data = i2c_recieve(); 
 		i2c_ack();
-		*data |= i2c_recv() << 8; 
+		*data |= i2c_recieve() << 8; 
 
 		i2c_nack();
 		i2c_stop();
